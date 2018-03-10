364e2d9695a491fe6fbd7556abb56ff178922198
Colin <s1546623@sms.ed.ac.uk>
2018-03-10 22:15:59 +0000

    Can now get list of device IDs currently in a team from database

--- app/src/main/java/hangryhippos/cappturetheflag/database/LiveGameConnection.java
+++ app/src/main/java/hangryhippos/cappturetheflag/database/LiveGameConnection.java
@@ -1,6 +1,7 @@
 package hangryhippos.cappturetheflag.database;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.mongodb.Block;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
@@ -12,7 +13,12 @@ import hangryhippos.cappturetheflag.database.obj.Team;
 import hangryhippos.cappturetheflag.database.obj.Utils;
 
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Projections.exclude;
+import static com.mongodb.client.model.Projections.excludeId;
+import static com.mongodb.client.model.Projections.fields;
+import static com.mongodb.client.model.Projections.include;
 import static com.mongodb.client.model.Updates.addToSet;
+import static com.mongodb.client.model.Updates.combine;
 import static com.mongodb.client.model.Updates.pull;
 
 /**
@@ -26,18 +32,16 @@ public class LiveGameConnection {
     private LatLng location;
     private String deviceID;
     private String displayName;
-    private MongoDatabase database;
+    private MongoCollection collection;
 
     public LiveGameConnection(LatLng location, String deviceID, String dislayName) {
         this.location = location;
         this.deviceID = deviceID;
         this.displayName = dislayName;
-        this.database = new DatabaseConnection().getMongoDatabase();
+        this.collection = new DatabaseConnection().getMongoDatabase().getCollection(GAME_SESSION_COLLECTION_NAME);
     }
 
     public void addPlayerToGame(Team team) {
-        MongoCollection collection = database.getCollection(GAME_SESSION_COLLECTION_NAME);
-
         Document memberDoc = new Document();
         memberDoc.put("deviceID", deviceID);
         memberDoc.put("displayName", displayName);
@@ -53,9 +57,26 @@ public class LiveGameConnection {
     }
 
     public void removePlayerFromGame() {
-        MongoCollection collection = database.getCollection(GAME_SESSION_COLLECTION_NAME);
         collection.findOneAndUpdate(eq("_id", 0), pull(Team.redTeam.name() + ".members", eq("deviceID", this.deviceID)));
         collection.findOneAndUpdate(eq("_id", 0), pull(Team.blueTeam.name() + ".members", eq("deviceID", this.deviceID)));
+    }
+
+    public ArrayList<String> getTeamMemberIDs(final Team team)
+    {
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        Document d = (Document) collection.find(eq("_id", 0)).projection(fields(include(team.name() + ".members.deviceID"), excludeId())).first();
+        Document teamDoc = (Document) d.get(team.name());
+        ArrayList<Document> deviceDocs = (ArrayList<Document>) teamDoc.get("members");
+
+        for(Document doc : deviceDocs)
+        {
+            arrayList.add(doc.getString("deviceID"));
+        }
+
+       return arrayList;
+
+
     }
 
 
