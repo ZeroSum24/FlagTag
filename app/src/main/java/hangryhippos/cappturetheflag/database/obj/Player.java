0654ea3ad1fa8cc47b1c855c1cc53334f462e500
Colin <s1546623@sms.ed.ac.uk>
2018-03-11 01:09:48 +0000

    New player class

---
+++ app/src/main/java/hangryhippos/cappturetheflag/database/obj/Player.java
@@ -0,0 +1,68 @@
+package hangryhippos.cappturetheflag.database.obj;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by colin on 11/03/18.
+ */
+
+public class Player
+{
+    private String deviceID;
+    private String displayName;
+    private Team team;
+    private boolean isJailed;
+    private int numOfCaps;
+    private int numOfTags;
+    private int numOfJails;
+    private String item;
+    private LatLng position;
+
+    public Player(String deviceID, String displayName, Team team, boolean isJailed, int numOfCaps, int numOfTags, int numOfJails, String item, LatLng position) {
+        this.deviceID = deviceID;
+        this.displayName = displayName;
+        this.team = team;
+        this.isJailed = isJailed;
+        this.numOfCaps = numOfCaps;
+        this.numOfTags = numOfTags;
+        this.numOfJails = numOfJails;
+        this.item = item;
+        this.position = position;
+    }
+
+    public String getDeviceID() {
+        return deviceID;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public boolean isJailed() {
+        return isJailed;
+    }
+
+    public int getNumOfCaps() {
+        return numOfCaps;
+    }
+
+    public int getNumOfTags() {
+        return numOfTags;
+    }
+
+    public int getNumOfJails() {
+        return numOfJails;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public LatLng getPosition() {
+        return position;
+    }
+}
