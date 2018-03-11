47c4295627bce2268270f6373ace88e74d041122
Charles <S1528211@sms.ed.ac.uk>
2018-03-11 01:35:42 +0000

    Whoops
    Broke help. Sorry

--- app/src/main/java/hangryhippos/cappturetheflag/HelpActivity.java
+++ app/src/main/java/hangryhippos/cappturetheflag/HelpActivity.java
@@ -2,12 +2,47 @@ package hangryhippos.cappturetheflag;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class HelpActivity extends AppCompatActivity {
+public class HelpActivity extends AppCompatActivity implements
+        View.OnClickListener {
+    private Integer pointer=0;
+    private String[] hintTitles={"Title1","Title2"};
+    private String[] hints={"Hint1","Hint2"};
+    private String hintTitleCurrant=hintTitles[pointer];
+    private String hintCurrant=hints[pointer];
+    private Button LeftButton;
+    private Button RightButton;
+    LeftButton.setOnClickListener(this);
+    RightButton.setOnClickListener(this);
+    TextView hintTitleTV = (TextView)findViewById(R.id.TitleText);
+    TextView hintTV = (TextView)findViewById(R.id.HintText);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        //updateTextView(0);
+    }
+    private void updateTextView(int i) {
+        if ((pointer + i > 0) && (pointer + i < hints.length)) {
+            pointer += i;
+            hintTitleTV.setText(hintTitles[pointer]);
+            hintTV.setText(hints[pointer]);
+        }
+    }
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.LeftButton:
+                updateTextView(-1);
+                break;
+            case R.id.RightButton:
+                updateTextView(1);
+                break;
+
+        }
     }
 }
